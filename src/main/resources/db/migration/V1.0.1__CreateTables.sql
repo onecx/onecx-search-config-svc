10df3259e42704d6160f271dd0d7b5faf3236978
msewnarain <michelle.sewnarain@capgemini.com>
2024-02-12 06:30:47 +0100

    feat: upate api anf db model

--- src/main/resources/db/migration/V1.0.1__CreateTables.sql
+++ src/main/resources/db/migration/V1.0.1__CreateTables.sql
@@ -1,15 +1,24 @@
-    create table search_config (
-       GUID varchar(255) not null,
-        OPTLOCK int4 not null,
-        creationDate timestamp,
-        creationUser varchar(255),
-        modificationDate timestamp,
-        modificationUser varchar(255),
-       api_version varchar(255),
-       application varchar(255),
-       page varchar(255),
-        name varchar(255),
-       fieldListVersion int4,
-       criteria_as_json text,
-        primary key (GUID)
-    );
+create table search_config
+(
+    GUID             varchar(255) not null,
+    OPTLOCK          int4         not null,
+    creationDate     timestamp,
+    creationUser     varchar(255),
+    modificationDate timestamp,
+    modificationUser varchar(255),
+    api_version      varchar(255),
+    application      varchar(255),
+    page             varchar(255),
+    name             varchar(255),
+    fieldListVersion int4,
+    values           text,
+    read_only        boolean,
+    advanced         boolean,
+    columns          text,
+    default_config   boolean,
+    global_config    boolean,
+    primary key (GUID)
+);
+
+alter table if exists search_config
+    add constraint ck_config unique (name, page, application);
